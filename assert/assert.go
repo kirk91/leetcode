972c379e08ba0a5b743063faaf4c235bfdb82ae3
alphapigger <steven.hl.0901@gmail.com>
2016-11-06 21:40:21 +0800

    add assert package

---
+++ assert/assert.go
@@ -0,0 +1,19 @@
+package assert
+
+import (
+	"log"
+	"reflect"
+)
+
+func Equal(expected, actual interface{}) bool {
+	if expected == nil || actual == nil {
+		return expected == actual
+	}
+	return reflect.DeepEqual(expected, actual)
+}
+
+func MustEqual(expected, actual interface{}) {
+	if !Equal(expected, actual) {
+		log.Fatalf("expected: %s, but received: %s", expected, actual)
+	}
+}
